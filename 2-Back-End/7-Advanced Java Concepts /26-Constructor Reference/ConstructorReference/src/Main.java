472a284f7649eae7cc4747d0099a8c865b35a846
MaherMahmoudSafwat <MaherMsSoftware2001@gmail.com>
2025-05-25 15:11:41 +0300

    Method reference

--- 2-Back-End/7-Advanced Java Concepts /26-Constructor Reference/ConstructorReference/src/Main.java
+++ 2-Back-End/7-Advanced Java Concepts /26-Constructor Reference/ConstructorReference/src/Main.java
@@ -2,20 +2,22 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-class Student
-{
+class Student {
     String Name;
     int Age;
-    Student(String Name,int Age)
-    {
+
+    // Constructor with Name and Age
+    Student(String Name, int Age) {
         this.Name = Name;
         this.Age = Age;
     }
 
-    Student(String Name)
-    {
+    // Constructor with just Name (Age will be default 0)
+    Student(String Name) {
         this.Name = Name;
     }
+
+    // Standard getters and setters
     public void setName(String name) {
         Name = name;
     }
@@ -32,6 +34,7 @@ class Student
         return Age;
     }
 
+    // Override toString() for better object printing
     @Override
     public String toString() {
         return "Student{" +
@@ -39,15 +42,37 @@ class Student
                 ", Age=" + Age +
                 '}';
     }
-
 }
-public class Main {
-    public static void main(String[] args)
-    {
-        List<String> names = Arrays.asList("Youssef","Mostafa","Omar");
-        List<Student>Students = new ArrayList<Student>();
-        Students = names.stream().map(Student::new).toList();
 
+public class Main {
+    public static void main(String[] args) {
+        // 1. Create a list of student names
+        List<String> names = Arrays.asList("Youssef", "Mostafa", "Omar");
+
+        // 2. Convert names to Student objects using Stream API
+        /*
+         * The stream pipeline:
+         *   - names.stream(): Creates a stream from the names list
+         *   - .map(Student::new): Transforms each name to a Student object
+         *     - Student::new is a constructor reference:
+         *       * Equivalent to: name -> new Student(name)
+         *       * Calls the Student(String Name) constructor for each name
+         *   - .toList(): Collects results into a new List (Java 16+ feature)
+         */
+        List<Student> Students = names.stream()
+                .map(Student::new)  // Constructor reference
+                .toList();          // Terminal operation
+
+        // 3. Print the resulting list of Students
+        /*
+         * Students.toString() will call each Student's toString() method
+         * due to the @Override we defined in the Student class
+         */
         System.out.println(Students);
     }
 }
+
+
+
+
+
